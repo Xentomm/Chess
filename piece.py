4a1117fd1a6e44db1953f257012675e8529aed6e
Xentomm <erimuslukasz@gmail.com>
2022-10-01 17:15:05 +0200

    piece_V1

--- piece.py
+++ piece.py
@@ -1,3 +1,4 @@
+import os
 
 class Piece:
 
@@ -7,12 +8,18 @@ class Piece:
 
         value_sign = 1 if color == 'white' else -1
         self.value = value * value_sign
+        self.moves = []
+        self.moves = False
         self.texture = texture
         self.set_texture()
         self.texture_rect = texture_rect
 
-    def set_texture():
-        pass
+    def set_texture(self, size = 80):
+        self.texture = os.path.join(
+            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png')
+
+    def add_moves(self, move):
+        self.moves.append(move)
 
 class Pawn(Piece):
 
