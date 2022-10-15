c91c6bf54924824443cbc239cbefd20e686b4527
Xentomm <erimuslukasz@gmail.com>
2022-10-15 16:57:34 +0200

    hover

--- main.py
+++ main.py
@@ -24,8 +24,10 @@ class Main:
         while True:
             #show methods
             game.show_bg(screen)
+            game.show_last_move(screen)
             game.show_moves(screen)
             game.show_pieces(screen)
+            game.show_hover(screen)
 
             if dragger.dragging:
                 dragger.update_blit(screen)
@@ -50,17 +52,24 @@ class Main:
 
                             # show methods
                             game.show_bg(screen)
+                            game.show_last_move(screen)
                             game.show_moves(screen)
                             game.show_pieces(screen)
 
                 # motion
                 elif event.type == pygame.MOUSEMOTION:
+                    motion_row = event.pos[1] // SQSIZE
+                    motion_col = event.pos[0] // SQSIZE
+                    game.set_hover(motion_row, motion_col)
+
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
                         # show methods
                         game.show_bg(screen)
+                        game.show_last_move(screen)
                         game.show_moves(screen)
                         game.show_pieces(screen)
+                        game.show_hover(screen)
                         dragger.update_blit(screen)
                 # free
                 elif event.type == pygame.MOUSEBUTTONUP:
@@ -81,6 +90,7 @@ class Main:
 
                             #show methods
                             game.show_bg(screen)  
+                            game.show_last_move(screen)
                             game.show_pieces(screen)      
 
                             # next turn
