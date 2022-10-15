d40a6bb21c6e9e58bd0dc7115c5a7bf237a087cd
Xentomm <erimuslukasz@gmail.com>
2022-10-15 17:36:08 +0200

    themes

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 from const import *
 from board import Board
 from dragger import Dragger
+from  config import Config
 
 class Game:
 
@@ -10,16 +11,16 @@ class Game:
         self.hovered_sqr = None
         self.board = Board()
         self.dragger = Dragger()
+        self.config = Config()
 
     # Show methods    
 
     def show_bg(self, surface):
+        theme = self.config.theme
+
         for row in range(ROWS):
             for col in range(COLS):
-                if (row + col) % 2 == 0:
-                    color = (234, 235, 200) # light green
-                else:
-                    color = (119, 154, 88) # dark green
+                color = theme.bg.light if (row + col) % 2 == 0 else theme.bg.dark
                 
                 rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
 
@@ -40,23 +41,27 @@ class Game:
                         surface.blit(img, piece.texture_rect)
     
     def show_moves(self, surface):
+        theme = self.config.theme
+
         if self.dragger.dragging:
             piece = self.dragger.piece
 
             for move in piece.moves:
-                color = '#C86464' if (move.final.row + move.final.col) % 2 == 0 else '#C84646'
+                color = theme.moves.light if (move.final.row + move.final.col) % 2 == 0 else theme.moves.dark
 
                 rect = (move.final.col * SQSIZE, move.final.row * SQSIZE, SQSIZE, SQSIZE)
 
                 pygame.draw.rect(surface, color, rect)
 
     def show_last_move(self, surface):
+        theme = self.config.theme
+
         if self.board.last_move:
             initial = self.board.last_move.initial
             final = self.board.last_move.final
 
             for pos in [initial, final]:
-                color = (244, 247, 116) if pos.row + pos.col % 2 == 0 else (172, 195, 51)
+                color = theme.trace.light if (pos.row + pos.col) % 2 == 0 else theme.trace.dark
 
                 rect = (pos.col * SQSIZE, pos.row * SQSIZE, SQSIZE, SQSIZE)
 
@@ -77,3 +82,6 @@ class Game:
 
     def set_hover(self, row, col):
         self.hovered_sqr = self.board.squares[row][col]
+
+    def change_theme(self):
+        self.config.change_theme()
